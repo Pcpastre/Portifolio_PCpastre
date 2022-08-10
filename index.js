ee8ba0cc67d989e32d2489907437ae9bd12ae682
Pcpastre <paulo.pastre_juninho@hotmail.com>
2022-08-10 07:56:59 -0300

    0.0.0

--- index.js
+++ index.js
@@ -3,11 +3,20 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = process.env.PORT || 80;
+const router = express.Router();
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'view/index.html'));
-})
+router.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname+'/view/index.html'));
+});
+
+app.get('/IMG', (req, res) =>{
+  var img = req.query.img;
+  res.sendFile(path.join(__dirname+'/view/_IMG/' + img));
+});
+
+//add the router
+app.use('/', router);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
-})
+});
